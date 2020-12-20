fb6d07fb96fb60800a6e19374badad26c94129fd
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-12-20 12:32:43 +0100

    long type annotations solved with Interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,12 +1,17 @@
+interface Vehicle {
+    name: string,
+    year: number,
+    isBroken: boolean
+}
 const oldCivic = {
     name: 'civic',
     year: 2000,
     isBroken: true
 }
 
-const printVehicle = (vehicle: { name: string; year: number; isBroken: boolean }): void => {
-    console.log(`name is: ${vehicle.name}`);
-    console.log(`was created at: ${vehicle.year}`);
-    console.log(`it is broken? ${vehicle.isBroken}`);
+const printVehicle = ({ name, year, isBroken }: Vehicle): void => {
+    console.log(`name is: ${name}`);
+    console.log(`was created at: ${year}`);
+    console.log(`it is broken? ${isBroken}`);
 }
 printVehicle(oldCivic);
