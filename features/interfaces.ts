5af813128e67e949633ddd7267d4cd7c678c86ca
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-12-20 13:10:29 +0100

    Code Reuse with Interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,22 +1,27 @@
-interface Vehicle {
-    name: string,
-    year: number | Date,
-    isBroken: boolean,
-    carDescription(): void
+interface Reportable {
+    summary(): string
 }
 const oldCivic = {
     novoDado: 2,//Não tem problema nenhum que esse objecto tenha propriedades a mais em relação a interface, desde que ele satisfaça a estrutura e os dados da interface está tudo bem.
     name: 'civic',
     year: new Date,
     isBroken: true,
-    carDescription(): void {
-        console.log('this is the car description');
+    summary(): string {
+        return `${this.name} is the name of the car`;
     }
 }
 
-const printVehicle = ({ name, year, isBroken }: Vehicle): void => {
-    console.log(`name is: ${name}`);
-    console.log(`was created at: ${year}`);
-    console.log(`it is broken? ${isBroken}`);
+const drinks = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar} grams of sugar`
+    }
 }
-printVehicle(oldCivic);
+
+const printReport = (report: Reportable): void => {
+    console.log(report.summary())
+}
+printReport(oldCivic);
+printReport(drinks);
