e32e8c3b4e6a1acb7ffdbdf5503f087bd10d616e
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-12-20 13:00:11 +0100

    Interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,12 +1,17 @@
 interface Vehicle {
     name: string,
-    year: number,
-    isBroken: boolean
+    year: number | Date,
+    isBroken: boolean,
+    carDescription(): void
 }
 const oldCivic = {
+    novoDado: 2,//Não tem problema nenhum que esse objecto tenha propriedades a mais em relação a interface, desde que ele satisfaça a estrutura e os dados da interface está tudo bem.
     name: 'civic',
-    year: 2000,
-    isBroken: true
+    year: new Date,
+    isBroken: true,
+    carDescription(): void {
+        console.log('this is the car description');
+    }
 }
 
 const printVehicle = ({ name, year, isBroken }: Vehicle): void => {
