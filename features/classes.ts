637ba68c4e0fb7a0257f11c9f1a69e9c44629fe2
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-12-21 12:04:02 +0100

    modifiers insides of arguments in constructor method and explanations

--- features/classes.ts
+++ features/classes.ts
@@ -26,8 +26,10 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-    constructor(color: string) {
+    //Nota: aqui não colocamos o modificador no argumento color porque é um atributo pertencente a classe pai, e não queremos reescrevê-lo, apenas utilizá-lo devido o método super, já o model(modelo do veiculo no caso) é um atributo pertencente a classe Car, por isso usamos o modificador public.
+    constructor(color: string, public model: number) {
         super(color);
+        this.model = model;
     }
     drive(): void {
         console.log("Overriting the method");
@@ -35,5 +37,5 @@ class Car extends Vehicle {
     }
 }
 
-const newCar = new Car('Blue');
+const newCar = new Car('Blue', 222);
 console.log(newCar.getColor());
