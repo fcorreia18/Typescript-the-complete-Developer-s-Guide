f6e5c135da2f7aa76782de96ba0afa3773e9ded7
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-12-21 11:49:58 +0100

    Classe

--- features/classes.ts
+++ features/classes.ts
@@ -1,15 +1,32 @@
 class Vehicle {
+    color: string;
+    constructor(color: string) {
+        this.setColor(color);
+    }
     drive(): void {
         console.log("something");
     }
     protected anotherMethod(): void {
         console.log("another method");
     }
+
+    setColor(color: string): void {
+        this.color = color;
+    }
+    getColor(): string {
+        return this.color;
+    }
 }
 
 class Car extends Vehicle {
+    constructor(color: string) {
+        super(color);
+    }
     drive(): void {
         console.log("Overriting the method");
         this.anotherMethod();
     }
 }
+
+const newCar = new Car('Blue');
+console.log(newCar.getColor());
