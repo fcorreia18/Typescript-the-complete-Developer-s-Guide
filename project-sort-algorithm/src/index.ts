4a2ef1254e323c86a75303d0bb62bc842079f639
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-12-24 08:42:24 +0100

    implementing the linkeList to work with our sorter class

--- project-sort-algorithm/src/index.ts
+++ project-sort-algorithm/src/index.ts
@@ -1,12 +1,21 @@
 import { Sort } from "./Sort";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 // const numbersCollection = new NumbersCollection([0, -2, 293, -17, 4]);
 // const sort = new Sort(numbersCollection);
 // sort.sort();
 // console.log(numbersCollection.data);//Para mostrar o array organizado de forma ascendente
 
-const charactersCollection = new CharactersCollection('aflkj');
-const sort = new Sort(charactersCollection);
+// const charactersCollection = new CharactersCollection('aflkj');
+// const sort = new Sort(charactersCollection);
+// sort.sort();
+// console.log(charactersCollection.data);  
+const linkedList = new LinkedList();
+linkedList.add(1234);
+linkedList.add(-43);
+linkedList.add(24);
+linkedList.add(4);
+const sort = new Sort(linkedList);
 sort.sort();
-console.log(charactersCollection.data);
+linkedList.print();
