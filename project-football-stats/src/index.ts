35f2e60e6b5c3f1fb618fdd67967404cbde35daa
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-12-26 13:28:19 +0100

    Loading the data from csv file

--- project-football-stats/src/index.ts
+++ project-football-stats/src/index.ts
@@ -2,5 +2,8 @@ import fs from 'fs';
 
 const data = fs.readFileSync("football.csv", {
     encoding: "utf-8"
-});
-console.log(data);
+})
+    .split('\n')
+    .map((rows: string): string[] => rows.split(','));
+
+
