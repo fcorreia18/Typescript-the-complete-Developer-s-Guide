4d5571246f4abbe82296018d7a7d7e3e5cdff077
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-01-16 21:41:33 +0100

    commenting the index

--- project-football-stats/src/index.ts
+++ project-football-stats/src/index.ts
@@ -4,8 +4,11 @@ import { Summary } from "./Summary";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { HtmlReport } from "./reportTargets/HtmlReport";
+
+//Create an object that satisfies the 'DataReader' interface
 const match = new CsvFileReader('football.csv');
 
+//Create an instance of 'MatchReader' and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader(match);
 matchReader.load();
 
