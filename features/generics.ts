86b8d4b2d502ba4af284a3c9d2d2f933af7059d3
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-02-16 08:10:52 +0100

    Function generics

--- features/generics.ts
+++ features/generics.ts
@@ -36,3 +36,46 @@ const qc = new ArrayOfAnything([9, 0, 6, 'p']);
 console.log(qc.get(1));
 
 
+
+
+function printString(arr: string[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+
+const printNumber = (arr: number[]): void => {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+const printAnything = <T>(arr: T[]): void => {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+
+function printAnything2<T>(arr: T[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+
+printString(["l", "k"]);
+printNumber([34, 89, 12]);
+printAnything<string>(['34, 89, 12', 's']);
+printAnything2<number>([2, 98]);
+
+
+
+
+
+
+
+
+
+
