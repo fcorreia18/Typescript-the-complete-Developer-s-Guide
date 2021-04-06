6c2b64fa25733e1f1b8a226a9e497aae308d9b57
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-04-06 10:16:39 +0100

    Decorators Factories

--- features/decorators.ts
+++ features/decorators.ts
@@ -4,13 +4,24 @@ class Boat {
     get formattedColor(): string {
         return ` This boat color is ${this.color}`;
     }
-
+    @logError("Oooops, boat was sunk with a new method")
     public pilot(): void {
+        throw new Error();
         console.log('swish');
     }
 }
 
-function testDecorator(target: any, key: string): void {
-    console.log("target:", target);
-    console.log("key:", key);
+function logError(message: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+        const method = desc.value;
+        desc.value = function (): void {
+            try {
+                method();
+            } catch (e) {
+                return console.log(message);
+            }
+        }
+    }
 }
+
+new Boat().pilot();
