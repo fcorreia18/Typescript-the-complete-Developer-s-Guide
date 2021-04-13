207b250787bf8e5dd6d84a807e935a8556537504
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-04-13 12:42:12 +0100

    Practical Metadata

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,13 +1,40 @@
 import 'reflect-metadata'
 
-const plane = {
-    color: "red"
-};
+// const plane = {
+//     color: "red"
+// };
 
-// Reflect.defineMetadata('note', 'hi there', plane);
-// const note = Reflect.getMetadata('note', plane);
-Reflect.defineMetadata('note', 'hi there', plane, 'color');
-const note = Reflect.getMetadata('note', plane, 'color');
+// // Reflect.defineMetadata('note', 'hi there', plane);
+// // const note = Reflect.getMetadata('note', plane);
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
+// const note = Reflect.getMetadata('note', plane, 'color');
 
 
-console.log(note);
+// console.log(note);
+@printMetadata
+class Plane {
+
+    color: string = "red";
+    @markFunction('hiiii')
+    public fly(): void {
+        console.log('vrrrrrrr');
+    }
+
+}
+
+
+function markFunction(secretInfo: string) {
+    return function (target: Plane, key: string) {
+        Reflect.defineMetadata('secret', secretInfo, target, key);
+    }
+}
+
+function printMetadata(target: typeof Plane) {
+    for (const key in target.prototype) {
+        const secret = Reflect.getMetadata('secret', target.prototype, key)
+        console.log(secret);
+    }
+}
+// const note = Reflect.getMetadata('note', Plane.prototype, 'fly');
+
+// console.log(note);
