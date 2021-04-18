6515a56cc059b86db17fa2d4638c31f6047ad0a3
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-04-18 13:11:55 +0100

    metadata keys with enum

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { Methods } from "./Methods";
+import { MetadataKeys } from "./MetadataKeys";
 
 export function controller(routePrefixer: string) {
     return function (target: Function) {
@@ -8,8 +9,8 @@ export function controller(routePrefixer: string) {
         const router = AppRouter.getInstance();
         for (let key in target.prototype) {
             const routeHandler = target.prototype[key];
-            const path = Reflect.getMetadata('path', target.prototype, key);
-            const method: Methods = Reflect.getMetadata('method', target.prototype, key);
+            const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
+            const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
 
             if (path) {
                 router[method](`${routePrefixer}${path}`, routeHandler);
