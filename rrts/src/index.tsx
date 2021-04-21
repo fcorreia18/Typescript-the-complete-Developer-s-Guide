ad15e621afbd50ae2c467c62256d9ab01ccc4579
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-04-21 11:12:47 +0100

    Functional Component

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -4,33 +4,35 @@ import ReactDOM from 'react-dom';
 interface AppProps {
     color?: string
 }
-
-interface AppState {
-    counter: number
+const App = (props: AppProps): JSX.Element => {
+    return <div>{props.color}</div>
 }
-class App extends React.Component<AppProps, AppState> {
-    // state: { counter: number } = { counter: 0 }
-    constructor(props: AppProps) {
-        super(props);
+// interface AppState {
+//     counter: number
+// }
+// class App extends React.Component<AppProps, AppState> {
+//     // state: { counter: number } = { counter: 0 }
+//     constructor(props: AppProps) {
+//         super(props);
 
-        this.state = { counter: 0 };
-    }
-    public onIncrement = (): void => {
-        this.setState({ counter: this.state.counter + 1 });
-    }
-    public onDecrement = (): void => {
-        this.setState({ counter: this.state.counter - 1 });
-    }
-    render() {
-        return (
-            <div>
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Decrement</button>
-                <div>{this.state.counter}</div>
-            </div>
+//         this.state = { counter: 0 };
+//     }
+//     public onIncrement = (): void => {
+//         this.setState({ counter: this.state.counter + 1 });
+//     }
+//     public onDecrement = (): void => {
+//         this.setState({ counter: this.state.counter - 1 });
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.onIncrement}>Increment</button>
+//                 <button onClick={this.onDecrement}>Decrement</button>
+//                 <div>{this.state.counter}</div>
+//             </div>
 
-        )
-    }
-}
+//         )
+//     }
+// }
 
 ReactDOM.render(<App color="red" />, document.getElementById('root'))
