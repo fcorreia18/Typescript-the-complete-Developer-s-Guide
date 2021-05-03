f8597a3321e6572106f3dbbbbafaffb11e42e427
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-03 10:11:26 +0100

    restarting the project, practicing, applying acquired knowledge

--- rrts/actions/index.ts
+++ rrts/actions/index.ts
@@ -1,13 +1,24 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionType } from "./types";
+export interface Todo {
+    id: number,
+    title: string,
+    completed: boolean
+}
+
+export interface fetchTodosAction {
+    type: ActionType.fetchTodos,
+    payload: Todo[]
+}
 
 const url = 'https://jsonplaceholder.typecode/todos';
 export const fetchTodos = (dispatch: Dispatch) => {
     return async () => {
-        const response = await axios.get(url);
+        const response = await axios.get<Todo[]>(url);
 
-        dispatch({
-            type: 'FETCH_TODOS',
+        dispatch<fetchTodosAction>({
+            type: ActionType.fetchTodos,
             payload: response.data
         })
     }
