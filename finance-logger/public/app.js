9bcf5b6edce103524bb6dc403b512d41e21d14e1
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-04 00:38:57 +0100

    the DOM and Type Casting

--- finance-logger/public/app.js
+++ finance-logger/public/app.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Olá');
+const anchor = document.querySelector('a');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+const form = document.querySelector('.new-item-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    return console.log(`${anchor.text}, ${type.value}, ${tofrom.value}, ${details.value}, ${amount.value}`);
+});
--- finance-logger/src/app.ts
+++ finance-logger/src/app.ts
@@ -1,1 +1,13 @@
-console.log('Olá')
+type INPUT = HTMLInputElement;
+const anchor: HTMLAnchorElement = document.querySelector('a')!;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as INPUT;
+const details = document.querySelector('#details') as INPUT;
+const amount = document.querySelector('#amount') as INPUT;
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    return console.log(`${anchor.text}, ${type.value}, ${tofrom.value}, ${details.value}, ${amount.value}`);
+})
