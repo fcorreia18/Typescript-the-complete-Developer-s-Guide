b0e26f057f472411ebbc769744d61bad7d880e08
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-03 13:58:16 +0100

    little bit of code refactor

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -4,16 +4,15 @@ import { Todo, fetchTodos } from '../actions'
 import { StoreState } from '../reducers'
 export interface AppProps {
     todos: Todo[],
-    fetchTodos(): any
+    fetchTodos(): Function
 }
 class _App extends Component<AppProps> {
     onButtonClick = (): void => {
         this.props.fetchTodos();
     }
     renderList(): JSX.Element[] {
-        return this.props.todos.map((todos: Todo): JSX.Element => {
-            return <div key={todos.id}>{todos.title}</div>
-        })
+        return this.props.todos.map((todos: Todo): JSX.Element => <div key={todos.id}>{todos.title}</div>
+        )
     }
 
 
